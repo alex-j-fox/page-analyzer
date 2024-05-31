c5eeed090f6430a7b9909015c2f422deb501e5b1
alex-j-fox <alex-j-fox@yandex.ru>
2024-05-31 11:21:35 +0300

    Refactor app.py
    Add render_template to index in page_analyzer/app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv, find_dotenv
-from flask import Flask
+from flask import Flask, render_template
 
 load_dotenv(find_dotenv())
 app = Flask(__name__)
@@ -10,7 +10,9 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def index():
-    return "Hello, Alex!!!"
+    return render_template(
+        'index.html'
+    )
 
 
 if __name__ == '__main__':
