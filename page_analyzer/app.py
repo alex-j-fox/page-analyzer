d6d7b94ebecd8af993ddec7274016943addc0193
alex-j-fox <alex-j-fox@yandex.ru>
2024-07-09 19:57:21 +0300

    Feat: app.py
    Оптимизирован импорт
    Удалена документация
    Исправлены методы на @app.route, @app.post (без указания методов в аргументах)
    Добавлен контекстный менеджер в обработчики
    Добавлена проверка на длину URL < 255
    Добавлены обработчики ошибок 404 и 500

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,100 +1,95 @@
 import os
 
-from flask import Flask, render_template, request, url_for, redirect, flash, \
-    get_flashed_messages, Response, session
-from validators import url
+from flask import (Flask, render_template, request, url_for, redirect, flash,
+                   Response, session, abort)
+from validators import url as validators_url
 
-from page_analyzer.db_queries import get_url_info, get_urls_list, get_url_id, \
-    insert_url_to_urls, get_url_checks, get_urls_list_with_check_data, \
-    insert_check_to_url_checks
-from page_analyzer.url_utils import normalize_url, is_exists_url_name
+from page_analyzer import db_queries as db
+from page_analyzer.utils import normalize_url, is_exists_url_name
+
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except FileNotFoundError:
+    pass
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index() -> str:
-    """Handles the GET request to the root URL.
-
-    :return: The rendered HTML template 'index.html' with the flashed messages.
-    :rtype: str
-    """
-    form_value = session.get('url', '')
-    session.pop('url', '')
-    messages: list[tuple[str, str]] = get_flashed_messages(
-        with_categories=True)
     return render_template(
         'index.html',
-        messages=messages,
-        form_value=form_value
     )
 
 
-@app.route('/urls/<int:id>', methods=['GET'])
+@app.route('/urls/<int:id>')
 def show_url_page(id: int) -> str:
-    """
-    Handles the GET request to the '/urls/<id>' URL.
-
-    :param id: The ID of the URL to show.
-    :return: str: The rendered HTML template 'show.html' with the URL info.
-    """
-    session.pop('url', '')
-    messages: list[tuple[str, str]] = get_flashed_messages(
-        with_categories=True)
-    current_url = get_url_info(id)
-    current_url_checks = get_url_checks(current_url.name)
+    with db.connect_db(app) as conn:
+        current_url = db.get_url_info(conn, id)
+        if not current_url:
+            abort(404)
+        current_url_checks = db.get_url_checks(conn, id)
     return render_template(
-        'show.html',
-        id=id,
-        name=current_url.name,
-        date=current_url.created_at,
-        checks=current_url_checks,
-        messages=messages
+        'urls/detail.html',
+        url=current_url,
+        checks=current_url_checks
     )
 
 
-@app.route('/urls/', methods=['GET'])
+@app.route('/urls/')
 def show_urls_page() -> str:
-    """
-    Handles the GET request to the '/urls/' URL.
-
-    :return: The rendered HTML template 'urls.html' with the list of URLs.
-    """
-    all_urls = get_urls_list_with_check_data()
+    with db.connect_db(app) as conn:
+        urls = db.get_urls_list_with_check_data(conn)
     return render_template(
-        'urls.html',
-        urls=all_urls
+        'urls/list.html',
+        urls=urls
     )
 
 
-@app.route('/urls/', methods=['POST'])
-def urls_post() -> Response:
+@app.post('/urls/')
+def handle_url_post_request() -> Response:
     url_value = request.form.get('url')
     session['url'] = url_value
-    if url(url_value):
-        normalized_url = normalize_url(url_value)
-        urls_list = get_urls_list()
-        if is_exists_url_name(normalized_url, urls_list):
-            flash('Страница уже существует', 'info')
-        else:
-            insert_url_to_urls(normalized_url)
-            flash('Страница успешно добавлена', 'success')
-        current_id = get_url_id(normalized_url)
-        return redirect(url_for('show_url_page', id=current_id))
-    else:
+    if len(url_value) > 255:
+        flash('URL превышает 255 символов', 'danger')
+        return redirect(url_for('index'))
+
+    if not validators_url(url_value):
         flash('Некорректный URL', 'danger')
         return redirect(url_for('index'))
 
+    with db.connect_db(app) as conn:
+        normalized_url = normalize_url(url_value)
+        urls_list = db.get_urls_list(conn)
+        if is_exists_url_name(normalized_url, urls_list):
+            flash('Страница уже существует', 'info')
+            url_id = db.get_url_id(conn, normalized_url)
+        else:
+            url_id = db.insert_url_to_urls(conn, normalized_url)
+            flash('Страница успешно добавлена', 'success')
+        return redirect(url_for('show_url_page', id=url_id))
 
-@app.route('/urls/<int:id>/checks', methods=['POST'])
+
+@app.post('/urls/<int:id>/checks')
 def check_url(id: int) -> Response:
-    insert_check_to_url_checks(id)
+    db.insert_check_to_url_checks(id)
     flash('Страница успешно проверена', 'success')
     # flash('Произошла ошибка при проверке', 'danger')
     return redirect(url_for('show_url_page', id=id))
 
+
+@app.errorhandler(404)
+def page_not_found():
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error():
+    return render_template('errors/500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=False)
